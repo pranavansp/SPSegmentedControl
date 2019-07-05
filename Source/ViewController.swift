cb859ed4f1521d82ee1b264c0f233f54875bb03e
Pranavan <pranavanps@gmail.com>
2019-07-05 14:20:53 +0530

    Update for Swift 4

--- Source/ViewController.swift
+++ Source/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SPCustomSegment.swift
 //  SegmentControllerSample
 //
 //  Created by Sivarajah Pranavan on 8/19/17.
@@ -8,54 +8,111 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
- 
-    @IBOutlet weak var matchView: UIView!
-    @IBOutlet weak var partyView: UIView!
+class SPCustomSegment: UIControl {
     
+    var buttons = [UIButton]()
+    var selector : UIView!
+    var selectedSegmentIndex = 0
     
-    @IBOutlet weak var nonLeadingContrains: NSLayoutConstraint!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func customSegmentValChnge(_ sender: CustomSegment) {
-        switch sender.selectedSegmentIndex {
-        case 0:
-            
-            UIView.animate(withDuration: 0.3, animations: {
-                self.nonLeadingContrains.constant = -600
-                self.matchView.transform = .identity
-                self.partyView.transform = .identity
-            })
-            break
-        case 1:
-            UIView.animate(withDuration: 0.3, animations: {
-                self.nonLeadingContrains.constant = -600
-                self.partyView.transform = CGAffineTransform(translationX: +200, y: 0)
-                let x = self.view.center.x - self.matchView.center.x
-                self.matchView.transform = CGAffineTransform(translationX: x, y: 0)
-            })
-            break
-        case 2:
-            UIView.animate(withDuration: 0.3, animations: {
-                self.partyView.transform = CGAffineTransform(translationX: +200, y: 0)
-                let x = self.view.center.x - self.matchView.center.x
-                self.matchView.transform = CGAffineTransform(translationX: x, y: 0)
-                self.nonLeadingContrains.constant = 97
-            })
-            break
-        default:
+    @IBInspectable
+    var borderWidth: CGFloat = 0 {
+        didSet{
+            layer.borderWidth = borderWidth
         }
     }
 
+    @IBInspectable
+    var borderColor : UIColor = UIColor.clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var commaSeparatedButtonTitle: String = "" {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable
+    var textColor : UIColor = .lightGray{
+        didSet{
+            updateView()
+        }
+    }
+    
+    @IBInspectable
+    var selectorColor : UIColor = .darkGray{
+        didSet{
+            updateView()
+        }
+    }
+    
+    @IBInspectable
+    var selectorTextColor : UIColor = .white {
+        didSet{
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        buttons.removeAll()
+        subviews.forEach {
+                $0.removeFromSuperview()
+        }
+        
+        let buttonTitles = commaSeparatedButtonTitle.components(separatedBy: ",")
+        
+        for buttonTitle in buttonTitles{
+            let button = UIButton(type: .system)
+            button.titleLabel?.textAlignment = .center
+            button.titleLabel?.numberOfLines = 0
+            button.setTitle(buttonTitle, for: .normal)
+            button.setTitleColor(textColor, for: .normal)
+            button.addTarget(self, action:#selector(buttonTapped(button:)) ,for: .touchUpInside)
+            buttons.append(button)
+        }
+        
+        buttons[0].setTitleColor(selectorTextColor, for: .normal)
+        let selectorWidth = frame.width / CGFloat(buttonTitles.count)
+        selector = UIView(frame: CGRect(x: 0, y: 0, width: selectorWidth, height: frame.height))
+        selector.layer.cornerRadius = frame.height/2
+        selector.backgroundColor = selectorColor
+        addSubview(selector)
+        
+        let stackView = UIStackView(arrangedSubviews: buttons)
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        stackView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        stackView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
+    }
+    
+    override func draw(_ rect: CGRect) {
+         layer.cornerRadius = frame.height/2
+    }
+    
+    @objc func buttonTapped(button : UIButton) {
+        for (buttonIndex, btn) in buttons.enumerated() {
+            btn.setTitleColor(textColor, for: .normal)
+            
+            if btn == button {
+                selectedSegmentIndex = buttonIndex
+                let selectorStartPosition = frame.width / CGFloat(buttons.count) * CGFloat(buttonIndex)
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.selector.frame.origin.x = selectorStartPosition
+                })
+                btn.setTitleColor(selectorTextColor, for: .normal)
+                
+            }
+        }
+        sendActions(for: .valueChanged)
+    }
 }
-
