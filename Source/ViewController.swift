3953ac8dc06ddfc870d8ad8a07c260edd23fe367
Pranavan <pranavanps@gmail.com>
2019-07-06 11:05:56 +0530

    Update ViewController.swift

--- Source/ViewController.swift
+++ Source/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SPCustomSegment.swift
+//  ViewController.swift
 //  SegmentControllerSample
 //
 //  Created by Sivarajah Pranavan on 8/19/17.
@@ -7,112 +7,33 @@
 //
 
 import UIKit
+import SVProgressHUD
+import Alamofire
 
-class SPCustomSegment: UIControl {
-    
-    var buttons = [UIButton]()
-    var selector : UIView!
-    var selectedSegmentIndex = 0
-    
-    @IBInspectable
-    var borderWidth: CGFloat = 0 {
-        didSet{
-            layer.borderWidth = borderWidth
-        }
-    }
+enum AlertType{
+    case SMS,Interuption,Disconnection
+}
 
-    @IBInspectable
-    var borderColor : UIColor = UIColor.clear {
-        didSet {
-            layer.borderColor = borderColor.cgColor
-        }
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
     
-    @IBInspectable
-    var commaSeparatedButtonTitle: String = "" {
-        didSet {
-            updateView()
-        }
-    }
-    
-    @IBInspectable
-    var textColor : UIColor = .lightGray{
-        didSet{
-            updateView()
-        }
-    }
-    
-    @IBInspectable
-    var selectorColor : UIColor = .darkGray{
-        didSet{
-            updateView()
-        }
-    }
-    
-    @IBInspectable
-    var selectorTextColor : UIColor = .white {
-        didSet{
-            updateView()
-        }
-    }
-    
-    func updateView() {
-        buttons.removeAll()
-        subviews.forEach {
-                $0.removeFromSuperview()
-        }
-        
-        let buttonTitles = commaSeparatedButtonTitle.components(separatedBy: ",")
-        
-        for buttonTitle in buttonTitles{
-            let button = UIButton(type: .system)
-            button.titleLabel?.textAlignment = .center
-            button.titleLabel?.numberOfLines = 0
-            button.setTitle(buttonTitle, for: .normal)
-            button.setTitleColor(textColor, for: .normal)
-            button.addTarget(self, action:#selector(buttonTapped(button:)) ,for: .touchUpInside)
-            buttons.append(button)
-        }
-        
-        buttons[0].setTitleColor(selectorTextColor, for: .normal)
-        let selectorWidth = frame.width / CGFloat(buttonTitles.count)
-        selector = UIView(frame: CGRect(x: 0, y: 0, width: selectorWidth, height: frame.height))
-        selector.layer.cornerRadius = frame.height/2
-        selector.backgroundColor = selectorColor
-        addSubview(selector)
-        
-        let stackView = UIStackView(arrangedSubviews: buttons)
-        stackView.axis = .horizontal
-        stackView.alignment = .fill
-        stackView.distribution = .fillEqually
-        addSubview(stackView)
-        
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        stackView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
-        stackView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        
-    }
-    
-    override func draw(_ rect: CGRect) {
-         layer.cornerRadius = frame.height/2
-    }
-    
-    @objc func buttonTapped(button : UIButton) {
-        for (buttonIndex, btn) in buttons.enumerated() {
-            btn.setTitleColor(textColor, for: .normal)
-            
-            if btn == button {
-                selectedSegmentIndex = buttonIndex
-                let selectorStartPosition = frame.width / CGFloat(buttons.count) * CGFloat(buttonIndex)
-                UIView.animate(withDuration: 0.3, animations: {
-                    self.selector.frame.origin.x = selectorStartPosition
-                })
-                btn.setTitleColor(selectorTextColor, for: .normal)
-                
+    @IBAction func segmentChange(_ sender: SPCustomSegment) {
+            switch sender.selectedSegmentIndex {
+            case 0:
+                print("sms Type")
+                break
+            case 1:
+                print("Interuption Type")
+                break
+            case 2:
+                print("Disconnection Type")
+                break
+            default:
+                break
             }
-        }
-        sendActions(for: .valueChanged)
     }
 }
